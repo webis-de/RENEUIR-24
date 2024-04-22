41226d81531828a90583a8be5ac2838230250921
Integer-Ctrl <fabianhof02@gmail.com>
2024-04-22 17:35:26 +0200

    feat: first attempt of fit training cross-encoder #2)

--- code/cross-encoder.py
+++ code/cross-encoder.py
@@ -8,6 +8,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from sentence_transformers import CrossEncoder, SentenceTransformer
 import torch
+from sentence_transformers import InputExample
 import sentence_transformers
 
 # For a full training on the msmarco dataset we decided to use the
@@ -21,18 +22,18 @@ import sentence_transformers
 raw_input = pd.read_json("../data/triples-ms-marco-tiny.jsonl.gz", encoding="utf-8", lines=True)
 df = []
 
-for _, i in df.iterrows():
+for _, i in raw_input.iterrows():
     df += [
-        {'query': i["query"],
-         'text': i["postive_document"],
-         'label': 1},
-        {'query': i["query"],
-         'text': i["negative_passage"],
-         'label': 0}
+        InputExample(texts=[i["query"], i["positive_document"]], label=1),
+    ]
+    df += [
+        InputExample(texts=[i["query"], i["negative_passage"]], label=0),
     ]
 
-training_data = Dataset.from_pandas(pd.DataFrame(df))
+# training_data = Dataset.from_pandas(pd.DataFrame(df))
 
+# print(training_data)
+# print(training_data[0])
 
 # The model we want to fine-tune
 checkpoint = "bert-base-uncased"
@@ -43,7 +44,10 @@ num_epochs = 1
 model = CrossEncoder(checkpoint, num_labels=1, max_length=512)
 
 # We create a DataLoader to load our train samples
-# train_dataloader = DataLoader(raw_datasets, shuffle=True, batch_size=train_batch_size)
+train_dataloader = DataLoader(df, shuffle=True, batch_size=train_batch_size)
+
+# Print the first entry in train_dataloader
+# print(next(iter(train_dataloader)))
 
 # We define our loss funtion
 loss_function = torch.nn.BCEWithLogitsLoss()
@@ -52,16 +56,39 @@ loss_function = torch.nn.BCEWithLogitsLoss()
 # TODO: write training loop
 
 '''
+for epoch in range(num_epochs):
+    model.model.train()
+    for i, batch in enumerate(train_dataloader):
+        # print(i, batch)
+        queries = batch["query"]
+        passages = batch["text"]
+        labels = batch["labels"]
+
+        # Forward pass
+        scores = model.predict([queries, passages], show_progress_bar=True)
+
+        # Compute loss
+        loss = loss_function(scores, labels)
+
+        # Backward pass
+        loss.backward()
+
+        # Update weights
+        model.optimizer.step()
+        model.optimizer.zero_grad()
+
+        print(f"Epoch: {epoch}, Loss: {loss.item()}")
+'''
+
 # Train the model
 model.fit(
     train_dataloader=train_dataloader,
-    evaluator=evaluator,
+    loss_fct=loss_function,
     epochs=num_epochs,
     evaluation_steps=10000,
-    warmup_steps=warmup_steps,
-    use_amp=True,
+    warmup_steps=5000,
 )
-'''
+
 
 # Save latest model
 model.save("cross-encoder-ms-marco-tiny")
