bb7e0d4c19921f87d2a63c85a758a00b30a4f907
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-03 07:51:47 +0200

    mf

--- data/bm25_scoring.py
+++ data/bm25_scoring.py
@@ -1,13 +1,26 @@
 import pyterrier as pt
 from os.path import abspath
+import pandas as pd
 
 if not pt.started():
     pt.init()
     index = pt.IndexRef.of(abspath('index'))
     index = pt.IndexFactory.of(index)
+    tokeniser = pt.autoclass("org.terrier.indexing.tokenisation.Tokeniser").getTokeniser()
 
-def bm25_score(qid, docnos):
+def pt_tokenise(text):
+    return ' '.join(tokeniser.getTokens(text))
+
+def bm25_score(query, docnos):
     global index
+    pipeline = pt.FeaturesBatchRetrieve(index, wmodel="TF", features=["WMODEL:BM25", "WMODEL:TF", "WMODEL:TF-IDF", "WMODEL:PL2", "WMODEL:DirichletLM"])
+
+    df = []
+    for docno in docnos:
+        df += [{
+            'qid': '1', 'query': pt_tokenise(query),
+            'docno': docno,
+        }]
 
     return None
 
