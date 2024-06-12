e3973b831d69b6b2625e5421a082efaa0fc64b5c
Maik Fr?be <kibi9872@webis10.medien.uni-weimar.de>
2024-06-12 13:26:29 +0200

    prepare distillation with lightning ir

--- code/lightning-ir/fit-model.sh
+++ code/lightning-ir/fit-model.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-lightning-ir fit --config config/tinybert.yaml
+lightning-ir fit --config config/tinybert.yaml --model.init_args.model_name_or_path ${1}
 
--- code/lightning-ir/training-slurm.sh
+++ code/lightning-ir/training-slurm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=reneuir_distillation_tinybert
-#SBATCH --gres=gpu:2g.10gb
+#SBATCH --gres=gpu:ampere
 #SBATCH --output=slurm-output/reneuir_distillation_tinybert.%j.out
 #SBATCH --error=slurm-output/reneuir_distillation_tinybert.%j.err
 #SBATCH --container-image=bash
@@ -20,7 +20,8 @@ echo $SLURM_JOB_NODELIST
 cd /mnt/ceph/storage/data-tmp/current/kibi9872/RENEUIR-24/code/lightning-ir
 export PYTHONUNBUFFERED=TRUE
 
-MODELS=("cat" "dog" "mouse" "frog")
+MODELS=("/mnt/ceph/storage/data-in-progress/data-research/web-search/RENEUIR-24/models--prajjwal1--bert-tiny/snapshots/6f75de8b60a9f8a2fdf7b69cbd86d9e64bcb3837" "/mnt/ceph/storage/data-in-progress/data-research/web-search/RENEUIR-24/models--Integer-Ctrl--cross-encoder-bert-tiny-1gb-bs32/snapshots/94de6cd2d169d5a0b15bb17021713ac1cb91e0ed" "/mnt/ceph/storage/data-in-progress/data-research/web-search/RENEUIR-24/models--Integer-Ctrl--cross-encoder-bert-tiny-512/snapshots/7415399968e780d904e7e9a7b84d20304b834ef7" "/mnt/ceph/storage/data-in-progress/data-research/web-search/RENEUIR-24/models--Integer-Ctrl--cross-encoder-bert-tiny-5120/snapshots/845964d41cd8b81a2433e74fb9766b7c9255ec1c" "/mnt/ceph/storage/data-in-progress/data-research/web-search/RENEUIR-24/models--Integer-Ctrl--cross-encoder-bert-tiny-51200/snapshots/3a588aa4b9fbd2e22f33cfb0095e837f38934364")
 MODEL="${MODELS[$SLURM_ARRAY_TASK_ID]}"
-echo "Run task: ${SLURM_ARRAY_TASK_ID} to train model ${MODEL}"
-#python3 train-cross-encoder-bm25-cat.py
+echo "Run task: ${SLURM_ARRAY_TASK_ID} to train model '${MODEL}'"
+export IR_DATASETS_HOME='/mnt/ceph/storage/data-tmp/current/fschlatt/.ir_datasets/'
+./fit-model.sh ${MODEL}
