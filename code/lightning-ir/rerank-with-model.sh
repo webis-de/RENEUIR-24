37111ca275cf5e006e1f5be3f43e222f378d976a
Maik Fr?be <kibi9872@webis10.medien.uni-weimar.de>
2024-06-24 11:14:25 +0200

    Add evaluation for lightning ir for ticket #11

--- code/lightning-ir/rerank-with-model.sh
+++ code/lightning-ir/rerank-with-model.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
 lightning-ir re_rank \
-	--config config/tinybert-inference.yaml \
-	--data.inference_datasets.init_args.run_path ${1}/rerank.jsonl.gz \
-	--trainer.callbacks.init_args.save_dir ${2}/ \
+	--config ${CONFIG_DIR}/tinybert-inference.yaml \
+	--data.inference_datasets.init_args.run_path ${inputDataset}/rerank.jsonl.gz \
+	--trainer.callbacks.init_args.save_dir ${outputDir}/ \
 	--model.init_args.model_name_or_path ${MODEL}
+
+mv ${outputDir}/rerank.run ${outputDir}/run.txt
 
