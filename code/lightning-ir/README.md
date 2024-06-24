910c3f07b48412b0f9039c54bed3f29c4f50742a
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-24 10:25:42 +0200

    prepare usage of lightning ir

---
+++ code/lightning-ir/README.md
@@ -0,0 +1,4 @@
+tira-run \
+	--input-dataset reneuir-2024/re-rank-spot-check-20240624-training
+	--image tiny-lightning-ir
+
--- code/lightning-ir/rerank-with-model.sh
+++ code/lightning-ir/rerank-with-model.sh
@@ -2,7 +2,7 @@
 
 lightning-ir re_rank \
 	--config config/tinybert-inference.yaml \
-	--data.inference_datasets.init_args.run_path msmarco-passage-trec-dl-2019-judged-20230107-training-rerank.jsonl.gz \
-	--trainer.callbacks.init_args.save_dir runs/${1}/ \
-	--model.init_args.model_name_or_path lightning_logs/${1}/huggingface_checkpoint/
+	--data.inference_datasets.init_args.run_path ${1}/rerank.jsonl.gz \
+	--trainer.callbacks.init_args.save_dir ${2}/ \
+	--model.init_args.model_name_or_path ${MODEL}
 
