899228bfc1e04352fd696071309a476f2ea435c3
Fabian Hofer <fabianhof02@gmail.com>
2024-06-26 11:11:25 +0200

    feat: training for ms-marco-TinyBER-L-2

--- code/training-slurm.sh
+++ code/training-slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=slurm-output/ir_project_training.%j.out
 #SBATCH --error=slurm-output/ir_project_training.%j.err
 #SBATCH --container-image=mam10eks/reneuir-tinybert:0.0.1
-#SBATCH --array=0-5
+#SBATCH --array=0-4
 #SBATCH --mem=60G
 #SBATCH --cpus-per-task=4
 #SBATCH --time=16:00:00
@@ -20,4 +20,5 @@ echo $SLURM_JOB_NODELIST
 cd $PWD/RENEUIR-24/code
 export PYTHONUNBUFFERED=TRUE
 echo "Run task: ${SLURM_ARRAY_TASK_ID}"
-python3 train-cross-encoder-bm25-cat.py
+python3 train-cross-encoder-bm25-cat-ms-marco-TinyBERT-L-2.py
+# python3 train-cross-encoder-bm25-cat-early-stopping-ms-marco-TinyBERT-L-2.py
