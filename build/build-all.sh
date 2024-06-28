7136e99a2f49bc7556932b855364529693d4df0e
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-29 01:23:15 +0200

    clean up

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -e
 
-docker build -t tiny-bert-ranker -f code/Dockerfile.tinybert --build-arg MODEL_NAME=webis/tiny-bert-ranker .
-docker build -t tiny-bert-ranker-l-2 -f code/Dockerfile.tinybert --build-arg MODEL_NAME=cross-encoder/ms-marco-TinyBERT-L-2 .
+docker build -t tiny-bert-ranker -f tiny-bert-ranker/Dockerfile --build-arg MODEL_NAME=webis/tiny-bert-ranker .
+docker build -t tiny-bert-ranker-l-2 -f tiny-bert-ranker/Dockerfile --build-arg MODEL_NAME=cross-encoder/ms-marco-TinyBERT-L-2 .
 
 docker build -t tiny-bert-ranker-bm25 -f tiny-bert-ranker-bm25/Dockerfile  --build-arg MODEL_NAME=webis/tiny-bert-ranker-bm25 .
 docker build -t tiny-bert-ranker-l2-bm25 -f tiny-bert-ranker-bm25/Dockerfile  --build-arg MODEL_NAME=webis/tiny-bert-ranker-l2-bm25 .
