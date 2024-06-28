751a863d6c4c8af317649a0073df98f064617fe2
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-29 01:27:54 +0200

    refactor

--- build/build-all.sh
+++ build/build-all.sh
@@ -6,7 +6,6 @@ docker build -t tiny-bert-ranker-l-2 -f tiny-bert-ranker/Dockerfile --build-arg 
 docker build -t tiny-bert-ranker-bm25 -f tiny-bert-ranker-bm25/Dockerfile  --build-arg MODEL_NAME=webis/tiny-bert-ranker-bm25 .
 docker build -t tiny-bert-ranker-l2-bm25 -f tiny-bert-ranker-bm25/Dockerfile  --build-arg MODEL_NAME=webis/tiny-bert-ranker-l2-bm25 .
 
-
-docker build -t tiny-bert-ranker-distillation -f code/lightning-ir/Dockerfile --build-arg MODEL_NAME=webis/tiny-bert-ranker-distillation .
-docker build -t tiny-bert-ranker-l-2-distillation -f code/lightning-ir/Dockerfile --build-arg MODEL_NAME=webis/tiny-bert-ranker-l-2-distillation .
+docker build -t tiny-bert-ranker-distillation -f tiny-bert-ranker-distillation/Dockerfile --build-arg MODEL_NAME=webis/tiny-bert-ranker-distillation .
+docker build -t tiny-bert-ranker-l-2-distillation -f tiny-bert-ranker-distillation/Dockerfile --build-arg MODEL_NAME=webis/tiny-bert-ranker-l-2-distillation .
 
